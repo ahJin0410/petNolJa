fdaa66cf2a83cfdfb44f0e42e78ccfd4a7c1c46c
AnHyejin <hyejinan0410@gmail.com>
2022-08-17 01:08:52 +0900

    2022/08/17 임시 비밀번호 메일 발송 완료ㅠㅠ!! 내일 화면 수정 예정

--- petNolJa/src/main/java/com/semi/petNolJa/member/find/controller/ModifyByPwdServlet.java
+++ petNolJa/src/main/java/com/semi/petNolJa/member/find/controller/ModifyByPwdServlet.java
@@ -1,17 +1,41 @@
 package com.semi.petNolJa.member.find.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.security.InvalidKeyException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
+import javax.activation.CommandMap;
+import javax.activation.MailcapCommandMap;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import javax.mail.Authenticator;
+import javax.mail.Message.RecipientType;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,6 +43,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.nbp.ncp.nes.ApiClient;
+import com.nbp.ncp.nes.ApiResponse;
+import com.nbp.ncp.nes.api.V1Api;
+import com.nbp.ncp.nes.auth.PropertiesFileCredentialsProvider;
+import com.nbp.ncp.nes.exception.ApiException;
+import com.nbp.ncp.nes.exception.SdkException;
+import com.nbp.ncp.nes.marshaller.FormMarshaller;
+import com.nbp.ncp.nes.marshaller.JsonMarshaller;
+import com.nbp.ncp.nes.marshaller.XmlMarshaller;
+import com.nbp.ncp.nes.model.EmailSendRequest;
+import com.nbp.ncp.nes.model.EmailSendRequestRecipients;
+import com.nbp.ncp.nes.model.EmailSendResponse;
 import com.semi.petNolJa.member.find.model.dto.MemberDTO;
 import com.semi.petNolJa.member.find.model.service.FindByIdAndPwdService;
 
@@ -76,36 +112,45 @@ public class ModifyByPwdServlet extends HttpServlet {
 //				e.printStackTrace();
 //			}
 		} else {
-			/* 이메일로 비밀번호 찾기 만들기~! */
-			String user = "hyejin_0410@naver.com";
-			String password = "test";
 			
-			Properties prop = new Properties();
-			prop.put("mail.smtp.user", user);
-			prop.put("mail.smtp.host", "smtp.naver.com");
-			prop.put("mail.smtp.port", 587);
-			prop.put("mail.smtp.auth", "true");
+			/* 메일 보내기는 조금 더 공부해보고 진행하기,,, */
+			ApiClient apiClient = new ApiClient.ApiClientBuilder()
+									  .addMarshaller(JsonMarshaller.getInstance())
+									  .addMarshaller(XmlMarshaller.getInstance())
+									  .addMarshaller(FormMarshaller.getInstance())
+									  .setCredentials(new PropertiesFileCredentialsProvider("C:\\1_NewPetNolJa_20220806ver\\petNolJa\\resources\\lib\\credentials.properties").getCredentials())
+									  .setLogging(true)
+									  .build();
+			V1Api apiInstance = new V1Api(apiClient);
 			
-			Session session = Session.getDefaultInstance(prop, new Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(user, password);
-				}
-			});
+			List<EmailSendRequestRecipients> esrrList = new ArrayList<EmailSendRequestRecipients>();
+			EmailSendRequestRecipients esrr = new EmailSendRequestRecipients();
+			esrr.setAddress(member.getMemberEmail());
+			esrr.setName(member.getMemberName());
+			esrr.setType("R");
+			esrrList.add(esrr);
+			
+			EmailSendRequest requestBody = new EmailSendRequest();
+			requestBody.setBody(member.getMemberName() + "님의 임시 비밀번호를 안내해 드립니다.\n <h1> [" + modifyPwd + "] </h1>");
+			requestBody.setRecipients(esrrList);
+			requestBody.setSenderAddress("hyejin_0410@naver.com");
+			requestBody.setSenderName("펫놀자[PetNolJa]");
+			requestBody.setTitle("펫놀자 회원님의 임시 비밀번호를 안내해 드립니다.");
+			requestBody.setConfirmAndSend(false);
+			
+			String X_NCP_LANG = "ko-KR";
+			System.out.println(requestBody);
 			
 			try {
-				MimeMessage message = new MimeMessage(session);
-				message.setFrom(new InternetAddress(user));
-				message.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(member.getMemberEmail()));
-				
-				message.setSubject("[PetNolJa] 임시 비밀번호를 알려드립니다.");
-				message.setText("회원님의 임시 비밀번호를 안내해 드립니다. \n [ " + modifyPwd + "]");
-				
-				Transport.send(message);
-				System.out.println("메일 전송 확인");
-			} catch (MessagingException e) {
+				ApiResponse<EmailSendResponse> result = apiInstance.mailsPost(requestBody, X_NCP_LANG);
+			} catch (ApiException e) {
+				int statusCode = e.getHttpStatusCode();
+				Map<String, List<String>> responseHeaders = e.getHttpHeaders();
+				InputStream byteSteam = e.getByteStream();
+				e.printStackTrace();
+			} catch (SdkException e) {
 				e.printStackTrace();
 			}
-			
 			PrintWriter out = response.getWriter();
 			out.print(new Gson().toJson(memberInfo));
 			out.flush();
@@ -133,4 +178,42 @@ public class ModifyByPwdServlet extends HttpServlet {
 		}
 		return randomPwd;
 	}
+	
+//	public String makeSignature() {
+//	    String space = " ";  // 공백
+//	    String newLine = "\n";  // 줄바꿈
+//	    String method = "POST";  // HTTP 메소드
+//	    String url = "/member/modify/pwd";  // 도메인을 제외한 "/" 아래 전체 url (쿼리스트링 포함)
+//	    String timestamp = "{timestamp}";  // 현재 타임스탬프 (epoch, millisecond)
+//	    String accessKey = "{accessKey}";  // access key id (from portal or sub account)
+//	    String secretKey = "{secretKey}";  // secret key (from portal or sub account)
+//
+//	    String message = new StringBuilder()
+//	        .append(method)
+//	        .append(space)
+//	        .append(url)
+//	        .append(newLine)
+//	        .append(timestamp)
+//	        .append(newLine)
+//	        .append(accessKey)
+//	        .toString();
+//
+//	    String encodeBase64String = null;
+//	    try {
+//	    SecretKeySpec signingKey = new SecretKeySpec(secretKey.getBytes("UTF-8"), "HmacSHA256");
+//	    Mac mac;
+//			mac = Mac.getInstance("HmacSHA256");
+//	    mac.init(signingKey);
+//
+//	    byte[] rawHmac = mac.doFinal(message.getBytes("UTF-8"));
+//	    encodeBase64String = Base64.getEncoder().encodeToString(rawHmac);
+//	    } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+//	    	e.printStackTrace();
+//	    } catch (InvalidKeyException e) {
+//			e.printStackTrace();
+//		}
+//	  return encodeBase64String;
+//
+//	}
+	
 }
